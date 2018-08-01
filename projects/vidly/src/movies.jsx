1f450c6c66342cfab5c844b27dc6557e3bc8ae10
Ivan <ivan@Ivans-MBP.hitronhub.home>
2018-08-01 10:46:27 -0700

    update

---
+++ projects/vidly/src/movies.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { getMovies } from "./services/fakeMovieService.js";
+class Movies extends Component {
+  state = { movies: getMovies() };
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>there is no movies</p>;
+    return (
+      <div className="container">
+        <p>there are {count} movies</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.deleteButton(movie)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  deleteButton = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies: movies });
+  };
+}
+export default Movies;
